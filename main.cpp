67b665930aded4d725c595cee7cbb3dc561ead5b
Tomas Krejci <tomas789@Tomass-iMac.local>
2013-05-14 23:39:28 +0200

    testing population crossover

--- main.cpp
+++ main.cpp
@@ -6,27 +6,40 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
 #include "node.h"
+#include "valuation.h"
 
 int 
 main(int argc, char * argv[]) {
 
-    auto t1 = random(3);
-    auto t2 = random(3);
+    valuation v;
 
-    t1->print(std::cout) << std::endl;
-    t2->print(std::cout) << std::endl;
+    std::vector<tree> t(10);
+    std::generate(t.begin(), t.end(), []() { return random(3); });
+
+    std::cout << " ### GENERATED ###" << std::endl;
+    std::for_each(t.begin(), t.end(), [&](tree & tmp) {
+        std::cout << tmp->eval(v) << " : ";
+        tmp->print(std::cout) << std::endl;
+    });
 
     //t1.random_subtree().first->print(std::cout) << std::endl;
 
-    tree::subtree_iterator i1 = t1.random_subtree();
-    tree::subtree_iterator i2 = t2.random_subtree();
+    for (int i = 0; i < 100; ++i) {
+        tree::subtree_iterator i1 = t[stochastic::get_max(t.size() - 1)].random_subtree();
+        tree::subtree_iterator i2 = t[stochastic::get_max(t.size() - 1)].random_subtree();
+        swap(i1, i2);
+    }
 
-    swap(i1, i2);
-
-    t1->print(std::cout) << std::endl;
-    t2->print(std::cout) << std::endl;
+    std::cout << std::endl << " ### CROSSED ###" << std::endl;
+    std::for_each(t.begin(), t.end(), [&](tree & tmp) {
+        std::cout << tmp->eval(v) << " : ";
+        tmp->print(std::cout) << std::endl;
+    });
 
     
     return 0;
